7e112ab375abdb1565c665d76c62705aa4fb29c3
Garcia Arribas <arribas@amazon.com>
2019-04-24 23:58:46 +0200

    Extract date from tweets that have it and add it to the json format

--- data_fetch/reverse_tweets.py
+++ data_fetch/reverse_tweets.py
@@ -1,15 +1,37 @@
+import datetime
 import json
+import re
 
-with open("all_tweets.json") as tweets:
+
+
+def extract_tweet(tweet):
+    media_urls = []
+    if tweet.has_key('extended_entities') and tweet['extended_entities'].has_key('media'):
+      for media in tweet['extended_entities']['media']:
+          media_urls.append(media['media_url'])
+
+    text = ''
+    date = ''
+    exp = re.split(r'^([0-9]?[0-9]/[0-9]?[0-9]/\d\d\d\d)[\s|:]\s*(.+)', tweet['full_text'])
+    print tweet['full_text']
+    print exp
+    if len(exp) > 1:
+        text = exp[2]
+        date = datetime.datetime.strptime(exp[1], '%d/%m/%Y').isoformat()
+    else:
+        text = exp[0]
+
+    return {'text': text, 'media': media_urls, 'date' : date}
+
+
+with open('all_tweets.json') as tweets:
     all_tweets = json.loads(tweets.read())
     reverse_tweets = reversed(all_tweets)
     result_tweets = []
+
     for tweet in reverse_tweets:
-        media_urls = []
-        if tweet.has_key("extended_entities") and tweet["extended_entities"].has_key("media"):
-            for media in tweet["extended_entities"]["media"]:
-                media_urls.append(media["media_url"])
-        result_tweets.append({"text": tweet["full_text"], "media": media_urls})
-    f = open("reversed_tweets.json", "w")
+        result_tweets.append(extract_tweet(tweet))
+
+    f = open('reversed_tweets.json', 'w')
     f.write(json.dumps(result_tweets))
     f.close()
